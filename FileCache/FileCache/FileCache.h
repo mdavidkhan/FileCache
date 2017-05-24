fbc976e2c550a2c9d24d58dee5d9a376c80e8ce7
Muhammad Dawood <muhammaddawood@humanfocus.co.uk>
2017-05-24 11:26:18 +0500

    Added the project

---
+++ FileCache/FileCache/FileCache.h
@@ -0,0 +1,19 @@
+//
+//  FileCache.h
+//  FileCache
+//
+//  Created by Muhammad Dawood on 5/24/17.
+//  Copyright © 2017 Muhammad Dawood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FileCache.
+FOUNDATION_EXPORT double FileCacheVersionNumber;
+
+//! Project version string for FileCache.
+FOUNDATION_EXPORT const unsigned char FileCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileCache/PublicHeader.h>
+
+
